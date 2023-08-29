e176ef1b9f5542e6cac736f3c7c0089433672da2
Thomas Roberts <thomas_roberts1@baylor.edu>
2023-08-29 15:20:07 -0500

    exercise2

--- script1.py
+++ script1.py
@@ -2,15 +2,15 @@
 # Correc the mistakes and produce the following output:
 # Johnny, the total of your purchase is $1,688.70
 
-def calcTotal(amt,tax,name):
-    mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
+def calcTotal(amt,thetax,myname):
+    mytotal = amt * (1+thetax)
+    print(f"{myname}, the total of your purchase is ${mytotal:,.2f}")
 
 myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+price = 1560
+thetax = 0.0825
 
-calcTotal(thetax,price,name)
+calcTotal(price,thetax,myname)
 
 
 
@@ -21,7 +21,8 @@ calcTotal(thetax,price,name)
 list1 = [1,'two',3.2,'four',5]
 
 for i in list1:
-    print(i)
+    if type(i) == int:
+        print(i)
 
 
 
